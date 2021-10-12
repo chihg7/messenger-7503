0b616ef12a1f809c47357cae17b3c19300787687
Chi <chihg7@gmail.com>
2021-10-11 22:45:54 -0700

    feature-03-multiple-user-chatting 2nd Commit

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -9,6 +9,8 @@ Conversation.belongsToMany(User, { through: 'ConversationParticipations' });
 User.belongsToMany(Conversation, { through: 'ConversationParticipations' });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
+Message.hasMany(User, { through: 'readBy' });
+User.hasMany(Message, { through: 'readBy' });
 
 module.exports = {
   User,
